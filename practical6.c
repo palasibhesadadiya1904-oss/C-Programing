362638cf91d648f5526f52af3026381357279684
palasibhesadadiya1904-oss <palasibhesadadiya1904@gmail.com>
2025-08-26 23:41:09 +0530

    Add files via upload

---
+++ practical6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+float celsius,fahrenheit;
+printf("enter temp in celsius:");
+scanf("%f",&celsius);
+fahrenheit=(celsius*9/5)+32;
+printf("temp in fahrenheit:%f",fahrenheit);
+return 0;
+
+
+
+
+
+
+
+
+
+}
