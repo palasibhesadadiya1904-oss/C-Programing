a70ab18070f27f6eee4bad239e50497d03349284
palasibhesadadiya1904-oss <palasibhesadadiya1904@gmail.com>
2025-11-21 02:30:06 +0530

    Add files via upload

---
+++ PR2.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+    int length=70,breadth=90,area,perimeter;
+    printf("enter the value of length:%d \n ",length);  
+     printf("enter the value of breadth:%d \n",breadth);
+    perimeter=2*(length+breadth);
+    printf("perimeter of the park:%d \n",perimeter);
+    area=(length*breadth);
+    printf(" area of the park :%d  \n",area);
+    printf("bhesdadiya palasi_25CE008");
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
